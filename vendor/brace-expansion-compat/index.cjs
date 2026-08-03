d15b90275a09b70d2d414e91bf92fc310a08e92a
TruelyCollectables <david@truelycollectables.com>
2026-08-03 11:37:46 -0600

    Route brace expansion compatibility through safe implementation

--- vendor/brace-expansion-compat/index.cjs
+++ vendor/brace-expansion-compat/index.cjs
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-require-imports -- CommonJS is required to preserve the legacy package API. */
 'use strict';
-const secureCore = require('brace-expansion-safe-core');
+const secureCore = require('@isaacs/brace-expansion');
 const expand = typeof secureCore === 'function' ? secureCore : secureCore.expand;
 if (typeof expand !== 'function') throw new TypeError('Patched core did not expose expand().');
 module.exports = expand;
