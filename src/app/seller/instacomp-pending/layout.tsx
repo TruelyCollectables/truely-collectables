dcd8a73c894a7a6fb6673248d3e335d472c40fd0
TruelyCollectables <david@truelycollectables.com>
2026-08-04 13:17:02 -0600

    Remove browser fetch interception from Pending Listings

--- src/app/seller/instacomp-pending/layout.tsx
+++ src/app/seller/instacomp-pending/layout.tsx
@@ -1,13 +1,12 @@
-import ChecklistIdentityGuard from "./ChecklistIdentityGuard";
 import ChecklistReadinessDashboard from "./ChecklistReadinessDashboard";
 
 export default function InstaCompPendingLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ChecklistIdentityGuard>
+    <>
       <ChecklistReadinessDashboard />
       {children}
-    </ChecklistIdentityGuard>
+    </>
   );
 }
