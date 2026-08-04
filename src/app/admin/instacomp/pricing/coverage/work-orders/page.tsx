681f7d63f49e527a53575b3b4795b01f86dda30e
TruelyCollectables <david@truelycollectables.com>
2026-08-04 06:47:47 -0600

    Add coverage work orders route

---
+++ src/app/admin/instacomp/pricing/coverage/work-orders/page.tsx
@@ -0,0 +1,8 @@
+import PrivatePricingWorkOrders from "../../_components/private-pricing-work-orders";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function PrivatePricingWorkOrdersPage() {
+  return <PrivatePricingWorkOrders />;
+}
