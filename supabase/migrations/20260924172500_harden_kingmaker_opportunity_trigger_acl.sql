82aff32c2d34a16116c094bd8032ddb4067302b6
David Bakanas <dagdankyholdings@outlook.com>
2026-09-24 11:22:53 -0600

    security(kingmaker): restrict opportunity trigger execution

---
+++ supabase/migrations/20260924172500_harden_kingmaker_opportunity_trigger_acl.sql
@@ -0,0 +1,5 @@
+revoke execute on function public.tcos_kingmaker_record_opportunity_event()
+from public, anon, authenticated;
+
+grant execute on function public.tcos_kingmaker_record_opportunity_event()
+to service_role;
